0af0ba7c625f5580e32d07ab8ba9084f41ec419d
Shinjinok <bartshin00@gmail.com>
2024-03-08 17:38:19 +0900

    dual ch47 fg

---
+++ Tools/autotest/fg_ch47_multi_view2.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#https://wiki.flightgear.org/Property_Tree/Native_Protocol_Slaving
+#./sim_vehicle.py -v ArduCopter -f heli -L PHNL --model flightgear2:127.0.0.1 --map --console
+#    --generic=socket,in,10,,5003,udp,MAVLink \
+#    --generic=socket,out,10,,5001,udp,MAVLink \
+#    { "AHRS_EKF_TYPE", 10 },
+#    { "INS_GYR_CAL", 0 },
+AUTOTESTDIR=$(dirname $0)
+
+nice fgfs \
+    --generic=socket,in,20,,9012,udp,MAVLink \
+    --generic=socket,out,20,,9013,udp,MAVLink \
+    --multiplay=out,10,127.0.0.1,8001 \
+    --multiplay=in,10,127.0.0.1,8000 \
+    --callsign=Test2 \
+    --model-hz=120 \
+    --aircraft=ch47 \
+    --fg-aircraft="$AUTOTESTDIR/aircraft" \
+    --lon=-157.926 \
+    --lat=21.3134 \
+    --geometry=650x550 \
+    --bpp=32 \
+    --disable-hud-3d \
+    --disable-horizon-effect \
+    --timeofday=noon \
+    --disable-fullscreen \
+    --disable-random-objects \
+    --disable-ai-models \
+    --fog-disable \
+    --disable-specular-highlight \
+    --disable-anti-alias-hud \
+    --wind=0@0 \
+    --prop:/sim/rendering/multithreading-mode=AutomaticSelection \
+    --prop:/sim/rendering/multithreading-mode=CullDrawThreadPerContext \
+    --prop:/sim/rendering/multithreading-mode=DrawThreadPerContext \
+    --prop:/sim/rendering/multithreading-mode=CullThreadPerCameraDrawThreadPerContext \
+    --prop:/sim/rendering/multi-samples=3 \
+    --prop:/sim/rendering/multi-sample-buffers=true \
+    $*
